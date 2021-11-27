d77a9a93d991d15b57c98a2a4c5185811cdaf0ee
Askarunro <paskar.y.a@gmail.com>
2021-11-27 17:15:51 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,7 +1,26 @@
 const refs = {
-    buttonStart:document.que
-}
-
+  bodyStyle: document.querySelector('body'),
+  buttonStart: document.querySelector('button[data-start]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+};
+let interval = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.buttonStart.addEventListener('click', () => {
+  interval = setInterval(() => {
+    const a = getRandomHexColor();
+    console.log(a);
+    refs.bodyStyle.style.backgroundColor = a;
+  }, 1000);
+  refs.buttonStart.setAttribute('disabled', 'disabled');
+  refs.buttonStop.removeAttribute('disabled');
+});
+
+refs.buttonStop.addEventListener('click', () => {
+  clearInterval(interval);
+  refs.buttonStop.setAttribute('disabled', 'disabled');
+  refs.buttonStart.removeAttribute('disabled');
+});
