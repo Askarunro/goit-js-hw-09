2593f8086e0397c25a4aa22b4766b4270eac5074
Askarunro <paskar.y.a@gmail.com>
2021-11-28 10:44:56 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,7 @@
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
+
+let dateChange = null;
 
 const options = {
   enableTime: true,
@@ -8,10 +9,16 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    options.defaultDate.setTime(selectedDates[0]);
+    dateChange = Date.parse(options.defaultDate);
+    // console.log(dateChange);
+    // console.log(selectedDates[0]);
+    inform();
+    buttonEn();
   },
 };
 
+// Доступ до елементів
 const refs = {
   input: document.querySelector('input[type="text"]'),
   button: document.querySelector('button[data-start]'),
@@ -20,33 +27,37 @@ const refs = {
   minutesSpan: document.querySelector('span[data-minutes]'),
   secondsSpan: document.querySelector('span[data-seconds]'),
 };
+// Виклик функції flatpickr на input зі значенням options
 flatpickr(refs.input, options);
 
+// Функція перевірки вибраної дати
+function inform() {
+  if (dateChange < Date.now()) {
+    alert('Please choose a date in the future');
+  }
+}
+// Функція додавання атрибуту 'disabled' до кнопки Start
 function buttonDis() {
   refs.button.setAttribute('disabled', 'disabled');
 }
 buttonDis();
+// Функція видалення атрибуту 'disabled' з кнопки Start
 function buttonEn() {
-  if (options.onClose) {
+  if (dateChange > Date.now()) {
     console.log(options.defaultDate);
     refs.button.removeAttribute('disabled');
   }
 }
-buttonEn();
-
-// console.log(options.defaultDate);
+// Додавання слухача події на кнопку Start з callback startTimer
 refs.button.addEventListener('click', startTimer);
+
+// Функція startTimer
 function startTimer() {
   const timer = {
     start() {
-      const startTime = 1638500000000;
       setInterval(() => {
         const currentTime = Date.now();
-        // console.log(currentTime);
-        const deltaTime = startTime - currentTime;
-        if (startTime < currentTime) {
-          alert('Please choose a date in the future');
-        }
+        const deltaTime = dateChange - currentTime;
         const { days, hours, minutes, seconds } = convertMs(deltaTime);
         refs.daysSpan.textContent = days;
         refs.hoursSpan.textContent = hours;
@@ -59,10 +70,12 @@ function startTimer() {
   timer.start();
 }
 
+// Функція додавання нуля перед значенням до двохзначного
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
+// Функція запису ms в значення дати,часу
 function convertMs(ms) {
   const second = 1000;
   const minute = second * 60;
@@ -77,6 +90,6 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
